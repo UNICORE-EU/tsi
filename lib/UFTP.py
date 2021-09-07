8e582fd0b6b59e47d2384a7510794bab706adc5c
Bernd Schuller <b.schuller@fz-juelich.de>
2021-09-07 16:55:27 +0200

    implement partial get/put

--- lib/UFTP.py
+++ lib/UFTP.py
@@ -4,42 +4,49 @@
 
 from ftplib import FTP
 from os import environ
+from pathlib import Path
 import sys
 
 from Utils import expand_variables, extract_parameter, run_command
+from asyncore import write
 
 def open_session(host, port, secret):
     ''' open an FTP session at the given UFTP server 
         and return the session object '''
     ftp = FTP()
     ftp.connect(host, port)
-    print(ftp.getwelcome())
-    print(ftp.login("anonymous", secret))
+    log_reply(ftp.getwelcome())
+    log_reply(ftp.login("anonymous", secret))
     return ftp
+
+def log_reply(reply):
+    print("uftpd: %s" % reply)
 
 def uftp(message, connector, config, LOG):
     """
-    (TBD: Launches a child process that) reads/writes a file via UFTP
+    Launches a child process that reads/writes a file via UFTP
     
     The message sent by the XNJS is scanned for:
-        TSI_UFTP_HOST     - UFTP server to connect to
-        TSI_UFTP_PORT     - UFTP listen port
-        TSI_UFTP_SECRET   - one-time password to use for logging in
-        TSI_UFTP_MODE     - GET, PUT
-        TSI_UFTP_REMOTE_FILE     - remote file to read from / write to
-        TSI_UFTP_LOCAL_FILE          - local file to read from / write to   
-        TSI_START         - start byte (defaults to '0')
-        TSI_LENGTH        - how many bytes to transfer (defaults to -1 i.e. the whole file)
+        TSI_UFTP_HOST        - UFTP server to connect to
+        TSI_UFTP_PORT        - UFTP listen port
+        TSI_UFTP_SECRET      - one-time password to use for logging in
+        TSI_UFTP_OPERATION   - GET, PUT
+        TSI_UFTP_WRITE_MODE  - FULL, PARTIAL
+        TSI_UFTP_REMOTE_FILE - remote file to read from / write to
+        TSI_UFTP_LOCAL_FILE  - local file to read from / write to   
+        TSI_UFTP_OFFSET      - start byte (defaults to '0')
+        TSI_UFTP_LENGTH      - how many bytes to transfer (defaults to -1 i.e. the whole file)
     """
 
     host = extract_parameter(message, 'UFTP_HOST')
     port = int(extract_parameter(message, 'UFTP_PORT'))
     secret = extract_parameter(message, 'UFTP_SECRET')
-    mode = extract_parameter(message, 'UFTP_MODE')
+    operation = extract_parameter(message, 'UFTP_OPERATION')
+    write_mode = extract_parameter(message, 'UFTP_WRITE_MODE', "FULL")
     remote_path = expand_variables(extract_parameter(message, 'UFTP_REMOTE_FILE'))
     local_path = expand_variables(extract_parameter(message, 'UFTP_LOCAL_FILE'))
-    start = int(extract_parameter(message, 'START', "0"))
-    length = int(extract_parameter(message, 'LENGTH', "-1"))
+    offset = int(extract_parameter(message, 'UFTP_OFFSET', "0"))
+    length = int(extract_parameter(message, 'UFTP_LENGTH', "-1"))
     
     uspace_dir = extract_parameter(message, "USPACE_DIR")
     stdout = extract_parameter(message, "STDOUT", "stdout")
@@ -52,12 +59,16 @@ def uftp(message, connector, config, LOG):
             "export UFTP_HOST=%s" % host,
             "export UFTP_PORT=%s" % port,
             "export UFTP_SECRET=%s" % secret,
-            "export UFTP_MODE=%s" % mode,
+            "export UFTP_OPERATION=%s" % operation,
+            "export UFTP_WRITE_MODE=%s" % write_mode,
+            "export UFTP_OFFSET=%s" % offset,
+            "export UFTP_LENGTH=%s" % length,
+            "export UFTP_WRITE_MODE=%s" % write_mode,
             "export UFTP_REMOTE_FILE=%s" % remote_path,
             "export UFTP_LOCAL_FILE=%s" % local_path,
             "export PYTHONPATH=%s" % environ["PYTHONPATH"],
             "cd %s" % uspace_dir,
-            "{ python3 %s > %s 2>&1 ; echo $? > %s ; } & echo $! > %s " % (uftp_client, stdout, exit_code_file, pid_file)
+            "{ python3 %s >> %s 2>&1 ; echo $? > %s ; } & echo $! > %s " % (uftp_client, stdout, exit_code_file, pid_file)
             ]
     
     cmd = ""
@@ -79,17 +90,38 @@ def main(argv=None):
     secret = environ['UFTP_SECRET']
     remote_path = environ['UFTP_REMOTE_FILE']
     local_path = environ['UFTP_LOCAL_FILE']
-    mode = environ['UFTP_MODE']
+    operation = environ['UFTP_OPERATION']
+    write_mode = environ['UFTP_WRITE_MODE']
+    offset = int(environ.get('UFTP_OFFSET', "0"))
+    length  = int(environ.get('UFTP_LENGTH', "-1"))
     
     print("Connecting to UFTPD %s:%s" % (host, port))
     
     ftp = open_session(host, port, secret)
     
-    if "GET"==mode:
+    partial = "PARTIAL"==write_mode
+     
+    if "GET"==operation:
         print("GET %s -> %s" % (remote_path, local_path))
-        with open(local_path, 'wb') as fp:
-            reply = ftp.retrbinary('RETR %s' % remote_path, fp.write)
-            print(reply)
+        if partial:
+            _mode = "r+b"
+            Path(local_path).touch()
+        else:
+            _mode = "wb"
+        with open(local_path, _mode) as fp:
+            if offset>0 or length>-1:
+                if partial:
+                    fp.seek(offset)
+                if length>-1:
+                    reply = ftp.sendcmd("RANG %s %s"% (offset, length))
+                    if not reply.startswith("350"):
+                        raise IOError("Error setting RANG: %s" % reply)
+                    log_reply(reply)
+            if length>-1:
+                reply = ftp.retrbinary('RETR %s' % remote_path, fp.write)
+            else:
+                reply = ftp.retrbinary('RETR %s' % remote_path, fp.write, rest=offset)
+            log_reply(reply)
             try:
                 ftp.quit()
             except:
@@ -97,8 +129,16 @@ def main(argv=None):
     else:
         print("PUT %s -> %s" % (local_path, remote_path))
         with open(local_path, 'rb') as fp:
+            if offset>0 or length>-1:
+                fp.seek(offset)
+                if not partial:
+                    offset = 0
+                reply = ftp.sendcmd("RANG %s %s"% (offset, length))
+                if not reply.startswith("350"):
+                    raise IOError("Error setting RANG: %s" % reply)
+                log_reply(reply)
             reply = ftp.storbinary('STOR %s' % remote_path, fp)
-            print(reply)
+            log_reply(reply)
             try:
                 ftp.quit()
             except:
